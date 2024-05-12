867d1068884d1d3ff62663567a8e4bbd46107396
Filip-Slepcev <filipslepcev@yahoo.ca>
2024-05-12 14:41:06 -0400

    removed commented code

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,15 +1,3 @@
-// import "dotenv/config";
-
-// /** @type { import("drizzle-kit").Config } */
-// export default {
-//     schema: "./src/db/schema.ts",
-//     driver: "pg",
-//     dbCredentials: {
-//         // eslint-disable-next-line no-undef
-//         connectionString: process.env.DB_URL,
-//     },
-// };
-
 import "dotenv/config";
 import type { Config } from "drizzle-kit";
 
